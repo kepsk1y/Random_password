b11714dbd3e8845833ca83964f974914d7a15355
kepsk1y <51197167+kepsk1y@users.noreply.github.com>
2021-11-11 18:19:11 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,11 @@
 import random
 import string 
 
+characters = string.ascii_letters + string.digits
+
 def get_random_string():
 	length = input("Enter the number of digits of password: ")
-	letters = string.ascii_lowercase
-	result_str = ''.join(random.choice(letters) for i in range(int(length)))
+	result_str = ''.join(random.choice(characters) for i in range(int(length)))
 
 	print(f"Random string of length {length} is - {result_str}")
 
